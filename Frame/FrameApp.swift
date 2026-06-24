b28f1fa0b9732222ffe8838a5c95cadfdd1354fa
Deng Jun <dengjun2006@zju.edu.cn>
2026-06-24 22:51:19 +0800

    Initial Commit

---
+++ Frame/FrameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameApp.swift
+//  Frame
+//
+//  Created by Jun Deng on 2026/6/24.
+//
+
+import SwiftUI
+
+@main
+struct FrameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
